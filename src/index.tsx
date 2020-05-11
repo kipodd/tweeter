3ea995bbd192ec49bc6aba9be3c1a1e730659760
kipodd <17299316+kipodd@users.noreply.github.com>
2020-05-11 12:07:46 +0300

    finished milestone 5

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFjgROHlFn_g8FHCLbMZRkv_F-Gi6D3FM",
+  authDomain: "kipodd-micro-blog.firebaseapp.com",
+  databaseURL: "https://kipodd-micro-blog.firebaseio.com",
+  projectId: "kipodd-micro-blog",
+  storageBucket: "kipodd-micro-blog.appspot.com",
+  messagingSenderId: "932225239116",
+  appId: "1:932225239116:web:e9a7c92088c6b3d3608ce9",
+  measurementId: "G-1NBTRSNBX3",
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
